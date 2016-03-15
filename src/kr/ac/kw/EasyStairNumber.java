df4462a7dee214854b7294dfa71d3592d0445322
jang <jetski0202@naver.com>
2016-03-15 21:15:16 +0900

    tuesday

--- src/kr/ac/kw/EasyStairNumber.java
+++ src/kr/ac/kw/EasyStairNumber.java
@@ -1,7 +1,40 @@
 package kr.ac.kw;
 
-public class EasyStairNumber {
+import java.util.Scanner;
+
+
+
+public class EasyStairNumber{
+	public static long mod = 1000000000L;
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
+		long[][] d = new long[n+1][10];
+		
+		//초기화
+		for(int i =1 ; i <= 9 ; i++){
+			d[1][i] = 1;
+		}
+		
+		for(int i = 2 ; i <= n ; i++){
+			for(int j = 0 ; j<= 9 ; j++){
+				d[i][j] = 0 ;
+				if(j - 1 >= 0){
+					d[i][j] += d[i-1][j-1];
+				}
+				if(j+1 <= 9){
+					d[i][j] += d[i-1][j+1];
+				}
+				d[i][j] %= mod;
+			}
+		}
+		long ans = 0 ; 
+		for(int  i = 0 ; i <= 9 ; i++){
+			ans += d[n][i] ;
+		}
+		
+		ans %= mod;
+		System.out.println(ans);
 	}
 }

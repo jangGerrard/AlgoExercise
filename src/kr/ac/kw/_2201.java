99c9fe4788fd43d1e8e69fe8e5135f37af304368
Jang <jetski0202@naver.com>
2016-03-28 11:48:01 +0900

    _2201

---
+++ src/kr/ac/kw/_2201.java
@@ -0,0 +1,24 @@
+package kr.ac.kw;
+
+import java.util.Scanner;
+
+public class _2201 {
+	
+	static String[] d ;
+	static int n ;
+	public static void  main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n  = sc.nextInt();
+		d = new String[n+1];
+		int first , last;
+		d[1] = "1";
+		first = last = 1;
+		int i = 0;
+		while(i != n ){
+			
+			
+			i++;
+		}
+	}
+}
